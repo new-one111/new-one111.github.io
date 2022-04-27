6f91e59543fade90f359fe328c9aee25d6723458
new-one111 <2049417673@qq.com>
2022-04-28 00:38:09 +0800

    Site updated: 2022-04-28 00:38:07

---
+++ js/FunnyTitle.js
@@ -0,0 +1,16 @@
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+         document.title = '呜呜呜~网站奔溃了';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+         document.title = '诶嘿~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
--- js/matery.js
+++ js/matery.js
@@ -135,6 +135,7 @@ $(function () {
         showOrHideNavBg(scroll);
     });
 
+
     function showOrHideNavBg(position) {
         let showPosition = 100;
         if (position < showPosition) {
@@ -170,3 +171,11 @@ $(function () {
     // 初始化加载 tooltipped.
     $('.tooltipped').tooltip();
 });
+
+    //黑夜模式提醒开启功能
+    setTimeout(function () {
+        if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
+            let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>晚上使用黑夜模式阅读能够减轻视觉疲劳。</span>'
+            M.toast({ html: toastHTML })
+        }
+    }, 2000)
